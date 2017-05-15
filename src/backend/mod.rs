26a21b5651f395354389c37f533401af283702ca
Andy Lowry <andy@andylowry.me.uk>
2017-05-15 15:59:44 +0100

    Use security framework on ios

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,10 +1,10 @@
 //! TLS backend-specific functionality.
 
-#[cfg(target_os = "macos")]
+#[cfg(any(target_os = "macos", target_os = "ios"))]
 pub mod security_framework;
 
 #[cfg(target_os = "windows")]
 pub mod schannel;
 
-#[cfg(not(any(target_os = "macos", target_os = "windows")))]
+#[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "ios")))]
 pub mod openssl;

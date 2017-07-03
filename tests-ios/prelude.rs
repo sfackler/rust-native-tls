ed0db25ac3e82affea28cd6a5ef7391d2e8a4135
Andy Lowry <andy@andylowry.me.uk>
2017-07-03 11:33:45 +0100

    Add p macro for tests

--- tests-ios/prelude.rs
+++ tests-ios/prelude.rs
@@ -1,3 +1,13 @@
 extern crate native_tls;
 
 pub use native_tls::*;
+
+
+macro_rules! p {
+    ($e:expr) => {
+        match $e {
+            Ok(r) => r,
+            Err(e) => panic!("{:?}", e),
+        }
+    }
+}

aa146ff59b4fb81ceb61616bef15a8048b8b09b2
Jakub Wieczorek <jakub.adam.wieczorek@gmail.com>
2019-07-13 00:21:23 +0200

    Fix warnings

--- src/imp/schannel.rs
+++ src/imp/schannel.rs
@@ -38,8 +38,8 @@ impl error::Error for Error {
         error::Error::description(&self.0)
     }
 
-    fn cause(&self) -> Option<&error::Error> {
-        error::Error::cause(&self.0)
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        error::Error::source(&self.0)
     }
 }
 

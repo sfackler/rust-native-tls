b5180f78f5eac835894127ab386f8f2684be04e3
rust1248 <109891485+rust1248@users.noreply.github.com>
2023-06-22 06:03:59 +0000

    Update builders documentation

--- src/lib.rs
+++ src/lib.rs
@@ -329,6 +329,8 @@ pub enum Protocol {
 }
 
 /// A builder for `TlsConnector`s.
+/// 
+/// You can get one from [`TlsConnector::builder()`](TlsConnector::builder)
 pub struct TlsConnectorBuilder {
     identity: Option<Identity>,
     min_protocol: Option<Protocol>,
@@ -517,6 +519,8 @@ impl TlsConnector {
 }
 
 /// A builder for `TlsAcceptor`s.
+/// 
+/// You can get one from [`TlsAcceptor::builder()`](TlsAcceptor::builder)
 pub struct TlsAcceptorBuilder {
     identity: Identity,
     min_protocol: Option<Protocol>,

bdedf02f48372efeccdf4323920c21bb1a044788
bytedream <bytedream@protonmail.com>
2023-07-20 13:10:47 +0200

    Use openssl on every platform

--- src/lib.rs
+++ src/lib.rs
@@ -104,16 +104,8 @@ use std::fmt;
 use std::io;
 use std::result;
 
-#[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "ios")))]
 #[macro_use]
 extern crate log;
-#[cfg(any(target_os = "macos", target_os = "ios"))]
-#[path = "imp/security_framework.rs"]
-mod imp;
-#[cfg(target_os = "windows")]
-#[path = "imp/schannel.rs"]
-mod imp;
-#[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "ios")))]
 #[path = "imp/openssl.rs"]
 mod imp;
 
